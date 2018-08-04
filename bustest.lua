6fe133ebf0cbcbfa73ad4a0e9a316b3392848ee4
webD97 <christian.danscheid@outlook.de>
2018-08-04 18:56:00 +0200

    Fix again

--- bustest.lua
+++ bustest.lua
@@ -1,14 +1,14 @@
-minetest = require("src.minetest")
+minetest = require("src.bustest.minetest")
 
-require "src.assertions.node.node_registered";
-require "src.assertions.node.node_has_description";
-require "src.assertions.node.node_has_top_texture";
-require "src.assertions.node.node_has_bottom_texture";
-require "src.assertions.node.node_has_left_texture";
-require "src.assertions.node.node_has_right_texture";
-require "src.assertions.node.node_has_front_texture";
-require "src.assertions.node.node_has_back_texture";
-require "src.assertions.node.node_has_alias";
+require "src.bustest.assertions.node.node_registered";
+require "src.bustest.assertions.node.node_has_description";
+require "src.bustest.assertions.node.node_has_top_texture";
+require "src.bustest.assertions.node.node_has_bottom_texture";
+require "src.bustest.assertions.node.node_has_left_texture";
+require "src.bustest.assertions.node.node_has_right_texture";
+require "src.bustest.assertions.node.node_has_front_texture";
+require "src.bustest.assertions.node.node_has_back_texture";
+require "src.bustest.assertions.node.node_has_alias";
 
-require "src.assertions.chat.chatmessage_sent_all";
-require "src.assertions.chat.chatmessage_sent_player";
+require "src.bustest.assertions.chat.chatmessage_sent_all";
+require "src.bustest.assertions.chat.chatmessage_sent_player";
